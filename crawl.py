dcf948f2c18fbae3638843a092449592d5c564ce
Michael Diamond@Aodh <dimo414@gmail.com>
2013-02-11 08:42:37 -0500

    Updates to checkers and doers

--- crawl.py
+++ crawl.py
@@ -4,9 +4,13 @@ Created on Feb 9, 2013
 @author: mdiamond
 '''
 
+import re
 from crawling.crawler import Crawler
 from crawling import checker, doer
 
 if __name__ == '__main__':
-    crawl = Crawler(checker.basic.isLocal, doer.basic.findInternalNoFollow())
-    crawl.crawl('http://www.digitalgemstones.com', 2)
+    #crawl = Crawler(checker.basic.isLocal, doer.basic.findInternalNoFollow())
+    #crawl.crawl('http://www.digitalgemstones.com', 2)
+    
+    crawl = Crawler(checker.basic.isLocal, doer.basic.findClass('external'))
+    crawl.crawl('http://en.wikipedia.org/wiki/Main_Page',1)
