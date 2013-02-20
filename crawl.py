9b930f8718b481a18f7c2a9c30b3c9fec85d444d
Michael Diamond <dimo414@gmail.com>
2013-02-20 02:31:31 -0500

    Better example description

--- crawl.py
+++ crawl.py
@@ -14,8 +14,8 @@ from crawling.crawler import Crawler
 from crawling import test, action
 
 if __name__ == '__main__':
-    """Sample crawl application.  Creates a crawler which will crawl local URLs and print linked article titles containing 'California'.
-    Specify a custom user agent (Chrome 24 on Win7) and throttle requests to 10 per minute.  Then crawl the english wikipedia, hitting
+    """Sample crawl application.  Creates a crawler which will crawl local URLs and print URLs with links containing 'California'.
+    Specify a custom user agent (Chrome 24 on Win7) and throttle requests to 10 per minute.  Then crawl the English Wikipedia, hitting
     the homepage and all pages linked to from that page, but no deeper."""
     crawl = Crawler(test.basic.isLocal, action.basic.tagContains('a', 'California'))
     crawl.setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17")
