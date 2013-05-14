d5bc990250e7ca279f274859dc1765feb64acd2b
Michael Diamond <dimo414@gmail.com>
2013-05-14 12:04:28 -0400

    isLocal fix

--- crawling/test.py
+++ crawling/test.py
@@ -23,7 +23,7 @@ class UrlTest:
     
     def isLocal(self, url):
         """Only crawl URLs  local to the current host."""
-        if url.startswith('//'):
+        if url.startswith('//') or url.startswith('mailto'):
             return False
         try:
             if url.index(':') < url.index('/'):
